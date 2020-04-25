4a28499bb0ccd60db4b5846eaef7d0123bea3a51
Viswa527 <Viswa527@github.com>
2020-04-25 13:38:56 +0530

    [Viswa]Refactor.modified using Switchcase statement

--- src/main/java/com/EmployeWagesProblem/Employee.java
+++ src/main/java/com/EmployeWagesProblem/Employee.java
@@ -6,17 +6,22 @@ public class Employee {
 	{
 		Random result = new Random();
 		int value= result.nextInt(3);
-		if(value==1)
+		switch(value)
 		{
-			System.out.println("Part Time Wages is :"+(int)(4*120));
-		}
-		else if(value==2)
-		{
-			System.out.println("Full Time Wages is "+(int)(8*120));
-		}
-		else
-		{
-			System.out.println("NO JOB--NO WAGES");
+			case 1:
+			{
+				System.out.println("Part Time Wages is :"+(int)(4*120));
+				break;
+			}
+			case 2:
+			{
+				System.out.println("Full Time Wages is "+(int)(8*120));
+				break;
+			}
+			default:
+			{
+				System.out.println("NO JOB--NO WAGES");
+			}
 		}
 	}
 }
