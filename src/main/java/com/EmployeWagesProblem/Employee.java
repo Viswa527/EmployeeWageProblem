64d0275f0db26d9864eadcc19f065e3ce08cb023
Viswa527 <Viswa527@github.com>
2020-04-25 13:18:47 +0530

    [Viswa]Added.Part Time Employee Wages

--- src/main/java/com/EmployeWagesProblem/Employee.java
+++ src/main/java/com/EmployeWagesProblem/Employee.java
@@ -4,8 +4,6 @@ import java.util.Random;
 public class Employee {
 	public static void main(String arg[])
 	{
-		System.out.println("Daily Wages is :"+(int)(8*120));
+		System.out.println("Part Time Wages is :"+(int)(8*120));
 	}
-	
-
 }
